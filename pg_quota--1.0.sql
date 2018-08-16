1ec128f977d5079f7edb02be5b3a871310952139
Shaoqi <sbai@pivotal.io>
2018-08-16 11:44:16 +0800

    Limit the amount of disk space that a table can use.

--- pg_quota--1.0.sql
+++ pg_quota--1.0.sql
@@ -7,17 +7,17 @@ CREATE SCHEMA quota;
 
 set search_path='quota';
 
-CREATE FUNCTION get_quota_status(rolid OUT oid, space_used OUT int8, quota OUT int8)
+CREATE FUNCTION get_quota_status(relid OUT oid, space_used OUT int8, quota OUT int8)
 RETURNS SETOF record STRICT
 AS 'MODULE_PATHNAME'
 LANGUAGE C;
 
 CREATE VIEW quota.status AS
-SELECT rolid::regrole AS rolname, space_used, quota
+SELECT relid::regclass AS tablename, space_used, quota
 FROM get_quota_status();
 
 -- Configuration table
-create table quota.config (roleid oid PRIMARY key, quota int8);
+create table quota.config (relationid oid PRIMARY key, quota int8);
 
 SELECT pg_catalog.pg_extension_config_dump('quota.config', '');
 
